d5a9ec681f42e4f05c7fdb114156674778916a6c
Ram Kishor <ram@omniesolutions.com>
2024-02-20 20:27:58 +0530

    ok

--- react-ui/src/views/cust/orders/Product.jsx
+++ react-ui/src/views/cust/orders/Product.jsx
@@ -6,6 +6,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import AddShoppingCart from '@mui/icons-material/AddShoppingCart';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import { ButtonGroup } from '@material-ui/core';
 import SellOutlinedIcon from '@mui/icons-material/SellOutlined';
 import defaultImg from '../../../assets/images/product/no-item-found.png'
@@ -21,12 +22,13 @@ export default function MediaCard(props) {
   return (
     <Card className="card" >
       <img
-        sx={{ height: 150, margin:1}}
+        sx={{ minHeight:150,  height: 150, margin:1}}
         src={
             item.logoUrl? item.logoUrl: defaultImg
         }
         
         title={item.title}
+        fullWidth
       />
       <CardContent style={{padding:'10px'}}>
         <Typography gutterBottom variant="h5" component="div">
@@ -38,11 +40,18 @@ export default function MediaCard(props) {
         <Typography variant="body2" color="text.secondary">
         {item.desciption}
         </Typography>
+
+        <Typography variant="body2" color="text.secondary">
+        <SellOutlinedIcon></SellOutlinedIcon>{price(item.retailPrice)} 
+          <del style={{color: 'GrayText', marginLeft:'10px'}}>{price(item.retailPrice)}</del>
+         
+        </Typography>
+
       </CardContent>
       <CardActions style={{padding:'10px'}}>
         <ButtonGroup fullWidth>
-          <Button variant='outlined' size="medium"><SellOutlinedIcon></SellOutlinedIcon>{price(item.retailPrice)} 
-          <del style={{color: 'GrayText', marginLeft:'10px'}}>{price(item.retailPrice)}</del>
+          <Button variant='outlined' size="medium" color='info'>
+            <ShoppingBagIcon></ShoppingBagIcon>
           </Button>
           <Button variant='contained' size="medium" color='secondary' onClick={()=> 
             addToCart(item)}><AddShoppingCart></AddShoppingCart></Button>
